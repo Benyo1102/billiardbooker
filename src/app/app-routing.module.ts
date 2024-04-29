948ebbe67b9574e657fbc8b372fa86ee58e95501
Imregi Bence Zsolt <82212669+Benyo1102@users.noreply.github.com>
2024-04-29 19:27:03 +0200

    routing edit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,13 @@ import { authGuard } from './shared/services/auth.guard';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: '/landing', pathMatch: 'full', canActivate: [authGuard], data: { needsLogin: false}},
+  { path: '', redirectTo: '/landing', pathMatch: 'full'},
   { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule), canActivate: [authGuard], data: { needsLogin: false}},
   { path: 'register', loadChildren: () => import('./pages/register/register.module').then(m => m.RegisterModule), canActivate: [authGuard], data: { needsLogin: false} },
   { path: 'gallery', loadChildren: () => import('./pages/gallery/gallery.module').then(m => m.GalleryModule), canActivate: [authGuard], data: { needsLogin: false}},
   { path: 'landing', loadChildren: () => import('./pages/landing/landing.module').then(m => m.LandingModule), canActivate: [authGuard], data: { needsLogin: false}},
   { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule), canActivate: [authGuard], data: { needsLogin: true } },
-  { path: '**', redirectTo: '/not-found', canActivate: [authGuard], data: { needsLogin: false}}
+  { path: '**', redirectTo: '/not-found'}
 ];
 
 @NgModule({
