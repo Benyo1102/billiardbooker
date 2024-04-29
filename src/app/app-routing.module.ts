8681a42fbd598da54649638b45a160248a5e788a
Imregi Bence Zsolt <82212669+Benyo1102@users.noreply.github.com>
2024-04-29 16:58:43 +0200

    login auth routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ const routes: Routes = [
   { path: 'register', loadChildren: () => import('./pages/register/register.module').then(m => m.RegisterModule) },
   { path: 'gallery', loadChildren: () => import('./pages/gallery/gallery.module').then(m => m.GalleryModule)},
   { path: 'landing', loadChildren: () => import('./pages/landing/landing.module').then(m => m.LandingModule)},
+  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule), canActivate: [AuthGuard]},
   { path: '**', redirectTo: '/not-found'}
 ];
 
