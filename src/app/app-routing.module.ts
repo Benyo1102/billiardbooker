29af91ff24f0a0862881313f6e900b831890ad59
Imregi Bence Zsolt <82212669+Benyo1102@users.noreply.github.com>
2024-04-29 19:23:43 +0200

    routing gurad

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,16 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ɵcompileNgModuleDefs } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { authGuard } from './shared/services/auth.guard';
 
 
 const routes: Routes = [
-  {path: '', redirectTo: '/landing', pathMatch: 'full'},
-  { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) },
-  { path: 'register', loadChildren: () => import('./pages/register/register.module').then(m => m.RegisterModule) },
-  { path: 'gallery', loadChildren: () => import('./pages/gallery/gallery.module').then(m => m.GalleryModule)},
-  { path: 'landing', loadChildren: () => import('./pages/landing/landing.module').then(m => m.LandingModule)},
+  { path: '', redirectTo: '/landing', pathMatch: 'full', canActivate: [authGuard], data: { needsLogin: false}},
+  { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule), canActivate: [authGuard], data: { needsLogin: false}},
+  { path: 'register', loadChildren: () => import('./pages/register/register.module').then(m => m.RegisterModule), canActivate: [authGuard], data: { needsLogin: false} },
+  { path: 'gallery', loadChildren: () => import('./pages/gallery/gallery.module').then(m => m.GalleryModule), canActivate: [authGuard], data: { needsLogin: false}},
+  { path: 'landing', loadChildren: () => import('./pages/landing/landing.module').then(m => m.LandingModule), canActivate: [authGuard], data: { needsLogin: false}},
   { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule), canActivate: [authGuard], data: { needsLogin: true } },
-  { path: '**', redirectTo: '/not-found'}
+  { path: '**', redirectTo: '/not-found', canActivate: [authGuard], data: { needsLogin: false}}
 ];
 
 @NgModule({
